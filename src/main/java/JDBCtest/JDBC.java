37b1ae85a17e4d2f8fcc8e8f7efa70e5ecce29c8
Igor <criokondor@mail.ru>
2020-02-11 22:14:16 +0300

    Adding some code for JDBC using

--- src/main/java/JDBCtest/JDBC.java
+++ src/main/java/JDBCtest/JDBC.java
@@ -1,28 +1,49 @@
+
+
 package JDBCtest;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
 import java.lang.*;
-import java.sql.SQLException;
-import java.sql.Statement;
 
 
-public class JDBC{
+public class JDBC {
 
-    public static void main(String[] args) throws ClassNotFoundException, SQLException, InstantiationException, IllegalAccessException{
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, InstantiationException, IllegalAccessException {
 
         String name = "root";
         String connectURL = "jdbc:mysql://localhost:3306/cars";
         String pass = "123";
-        Class.forName ("com.mysql.jdbc.Driver").newInstance ();
+        Class.forName("com.mysql.jdbc.Driver").newInstance();
         try (Connection conn = DriverManager.getConnection(connectURL, name, pass);
-             Statement statement = conn.createStatement()){
+             Statement statement = conn.createStatement()) {
+            // executeUpdate служит для добавления данных
+            //statement.executeUpdate( "drop table cars2;");
+            statement.executeUpdate("create table if not exists cars2 (id int(5) not null AUTO_INCREMENT, name_cars VARCHAR(30), name_user varchar (30), primary key (id));");
             statement.executeUpdate("INsert INTO cars2 (name_cars, name_user) VALUES ('Nissan', 'Petr')");
+            //execute служит для извлечения данных
+            // statement.execute("select table1.username from table1" +
+            //       " inner JOIN table2 on table1.user_id=table2.user_id inner JOIN table3 on table2.phone_id=table3.phone_id where room_number='30'");
+            ResultSet set = statement.executeQuery("select * from table2;");
+            //statement.executeQuery("select table1.username from table1 inner JOIN table2 on table1.user_id=table2.user_id inner JOIN table3 on table2.phone_id=table3.phone_id where room_number='30'");
+
+            while (set.next()) { // Тут сет закроется
+                System.out.println(set.getInt("user_id")); //вывод резалтСета через имя или номер колонки
+                System.out.println(set.getInt(1));
+                System.out.println("------");
+            }
+
+            ResultSet set1 = statement.executeQuery("select table1.username from table1 inner JOIN table2 on table1.user_id=table2.user_id inner JOIN table3 on table2.phone_id=table3.phone_id where room_number='30'");
+            while (set1.next()) {
+                System.out.println(set1.getString(1));
+            }
+            String userId = "1";
+            ResultSet set2 = statement.executeQuery("select * from table2 where user_id = '" +userId +"'");
+            while (set2.next()) {
+                System.out.println("phone_id: " + set2.getInt( "phone_id"));
+                System.out.println("pfone_number: " + set2.getInt("phone_number"));
+
+            }
         }
-
-
-
-
 
     }
 }
